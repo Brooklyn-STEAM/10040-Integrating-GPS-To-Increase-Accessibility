12e1b3f97db8355769858f7b6959ff1fff2e61dc
Kyle Heaven <kheaven@brooklynsteamcenter.org>
2025-02-14 08:57:46 -0500

    Sign in and sign up

--- main.py
+++ main.py
@@ -62,6 +62,102 @@ def connect_db():
 def index():
     return render_template("homepage.html.jinja")
 
+@app.route("/sign_up", methods =["POST", "GET"])
+def sign_up_page():
+    if flask_login.current_user.is_authenticated:
+        return redirect("/")
+    else:
+        if request.method == 'POST':
+
+            first_name = request.form["first_name"]
+            last_name = request.form["last_name"]
+
+            email = request.form["email"]
+            address = request.form["address"]
+
+            username = request.form["user_name"]
+            password = request.form["password"]
+            age = request.form["age"]
+
+            phone_number = request.form["phone_number"]
+
+            if request.form["roll"] == "Yes":
+                role = "Caretaker"
+            else: 
+                role = "Need Of Assistance"
+
+            if request.form["with_parent"] == "Yes":
+                with_parent = "Yes"
+            else:
+                with_parent = "No"
+
+            comfirm_password = request.form["verify_password"]
+
+            conn = connect_db()
+
+            cursor = conn.cursor()
+
+            if comfirm_password != password:
+                flash("Sorry, those passwords do not match")
+            elif len(password) < 12:
+                flash("Sorry, that password is too short")
+            elif (age <= 18 and with_parent == "No"):
+                flash("Must have a parent or gaurdian present")
+            else:
+                try:
+                    cursor.execute(f""" 
+                        INSERT INTO `User`
+                            (`first_name`, `last_name`, `username`, `password`, `email`, `address`, `age`, `phone_number`, `role`, `with_parent` )
+                        VALUES
+                            ( '{first_name}', '{last_name}', '{username}', '{password}', '{email}', '{address}', '{age}', '{phone_number}', '{role}', '{with_parent}' );
+                    """)
+                except pymysql.err.IntegrityError:
+                    flash("Sorry, that username/email is already in use")
+                else:
+                    return redirect("/sign_in") 
+                finally:
+                    cursor.close()
+                    conn.close()
+
+
+        return render_template("sign_up.html.jinja")
+
+
+@app.route("/sign_in", methods =["POST", "GET"])
+def sign_in_page():
+    if flask_login.current_user.is_authenticated:
+        return redirect("/")
+    else:
+        if request.method == "POST":
+            username = request.form['username'].strip()
+            password = request.form['password']
+
+            conn = connect_db()
+            cursor = conn.cursor()
+
+            cursor.execute(f"SELECT * FROM `User` WHERE `username` = '{username}';")
+
+            result = cursor.fetchone()
+
+            if result is None:
+                flash("Your username/password is incorrect")
+            elif password != result["password"]:
+                flash("Your username/password is incorrect")
+            else:
+                user = User(result["id"], result["username"], result["email"], result["first_name"], result["last_name"])
+                flask_login.login_user(user)
+
+                return redirect("/")
+
+
+        return render_template("sign_in.html.jinja")
+
+
+@app.route('/sign_out')
+def sign_out():
+    flask_login.logout_user()
+    return redirect('/')
+
 
 @app.route("/maps")
 def maps():
